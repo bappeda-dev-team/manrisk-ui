236d1c6ef94b07ef5f5bf8d65c55abdcace4d93a
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-06-21 17:32:42 +0700

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  basePath: "/manrisk",
+  assetPrefix: "/manrisk/",
   images: {
-    domains: ['cdnkk.zeabur.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdnkk.zeabur.app',
+        pathname: '**'
+      }
+    ]
   }
 };
 
